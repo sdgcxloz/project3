b5988f8dce8614ca9b810597b529169e8be90c80
sdgcxloz <sofia.rusmanova@gmail.com>
2026-04-17 12:34:14 +0300

    Refactor player join logic and drawing function

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
 let playerKey = null;
+
 async function join() {
-   const res = await fetch("https://tinkr.tech/sdb/poly/wander", {
-       method: "POST",
-       headers: {"Content-Type": "application/json"},
-       body: JSON.stringify({
-           action: "join",
-           username: "student_" + Math.floor(Math.random() * 1000)
-       })
-   });
+    const res = await fetch("https://tinkr.tech/sdb/poly/wander", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            action: "join",
+            username: "user" + Math.floor(Math.random() * 1000)
+        })
+    });
 
-   const data = await res.json();
-   playerKey = data.player_key;
+    const data = await res.json();
+    playerKey = data.player_key;
 
-   console.log("Joined with key:", playerKey);
+    console.log("joined", playerKey);
 }
 
 async function getWorld() {
-   const res = await fetch("https://tinkr.tech/sdb/poly/wander");
-   return await res.json();
+    const res = await fetch("https://tinkr.tech/sdb/poly/wander");
+    return await res.json();
 }
+
 function draw(world) {
-   const game = document.getElementById("game");
-   game.innerHTML = "";
+    const game = document.getElementById("game");
+    game.innerHTML = "";
 
-   for (let p of world.players) {
-       const div = document.createElement("div");
-       div.className = "player";
-       div.style.left = p.x + "px";
-       div.style.top = p.y + "px";
+    for (let p of world.players) {
+        const div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.left = p.x + "px";
+        div.style.top = p.y + "px";
 
-       const img = document.createElement("img");
-       img.src = p.image;
-       img.width = p.width;
-       img.height = p.height;
+        const img = document.createElement("img");
+        img.src = p.image;
+        img.width = p.width;
+        img.height = p.height;
 
-       const name = document.createElement("div");
-       name.textContent = p.username;
+        const name = document.createElement("div");
+        name.textContent = p.username;
 
-       div.appendChild(img);
-       div.appendChild(name);
-       game.appendChild(div);
-   }
+        div.appendChild(img);
+        div.appendChild(name);
+        game.appendChild(div);
+    }
 }
+
 async function start() {
-   await join();
+    await join();
 
-   setInterval(async () => {
-       const world = await getWorld();
-       draw(world);
-   }, 1000);
+    setInterval(async () => {
+        const world = await getWorld();
+        draw(world);
+    }, 1000);
 }
+
 start();
